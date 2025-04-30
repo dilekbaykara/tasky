6a68befb622cc66e314cde6f652586f229761266
Dilek Baykara <dilekbaykara@Dileks-MacBook-Pro.local>
2025-04-29 20:36:41 -0400

    added dependencies

--- app/src/main/java/com/dilekbaykara/tasky/MainActivity.kt
+++ app/src/main/java/com/dilekbaykara/tasky/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dilekbaykara.tasky
 
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,17 +12,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-
-
 import androidx.compose.foundation.layout.padding
-
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -32,9 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dilekbaykara.tasky.ui.theme.TaskyTheme
