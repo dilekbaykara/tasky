35b7a45d6f8c3693eb4a409939f0cfae87d29347
Dilek Baykara <dilekbaykara@Dileks-MacBook-Pro.local>
2025-05-10 23:16:01 -0400

    started AgendaItem structure

---
+++ app/src/main/java/com/dilekbaykara/tasky/domain/repository/AgendaRepositoryImpl.kt
@@ -0,0 +1,45 @@
+package com.dilekbaykara.tasky.domain.repository
+
+import com.dilekbaykara.tasky.data.repository.AgendaRepository
+import com.dilekbaykara.tasky.domain.model.AgendaItem
+import com.dilekbaykara.tasky.domain.model.AgendaItemType
+import com.dilekbaykara.tasky.presentation.agenda.AgendaItem
+
+class AgendaRepositoryImpl : AgendaRepository {
+    override suspend fun getAgendaItems(date: Long) : List<AgendaItem> {
+        return listOf(
+            AgendaItem(
+                AgendaItemType.Reminder,
+                "Eat Food",
+                "Cheese",
+                "May",
+                9,
+                10
+            ),
+            AgendaItem(
+                AgendaItemType.Task,
+                "Brush Hair",
+                "Use Hair oil",
+                "May",
+                11,
+                12
+            ),
+            AgendaItem(
+                AgendaItemType.Event,
+                "Eat container of cookies",
+                "Oreo's.. don't forget the milk",
+                "May",
+                13,
+                2
+            )
+        )
+    }
+
+    override suspend fun updateAgendaItems(id: String) {
+
+    }
+
+    override suspend fun deleteAgendaItems() {
+
+    }
+}
---
+++ app/src/main/java/com/dilekbaykara/tasky/presentation/agenda/Agenda.kt
@@ -0,0 +1,36 @@
+package com.dilekbaykara.tasky.presentation.agenda
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.dilekbaykara.tasky.presentation.auth.register.Header
+
+
+@Composable
+fun DatePickerCarousel(){
+    Surface() { }
+}
+
+@Composable
+fun DateHeader(){
+    Header()
+}
+
+@Composable
+fun AgendaItemList(){
+    LazyColumn {
+
+    }
+}
+
+@Composable
+fun AgendaItem(){
+
+}
+
+@Composable
+fun AddAgendaItemFab(){
+
+}
+
+
